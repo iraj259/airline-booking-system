8d5d2a48b877d8c41913dd5bf1b139672e92c72e
iraj259 <irajj.259@gmail.com>
2025-10-14 13:10:24 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const {PORT} = require('./config')
+const {ServerConfig, Logger} = require('./config')
 const apiRoutes =require('./routes')
 const app = express()
 
 app.use('/api',apiRoutes)
 
-app.listen(PORT,()=>{
-    console.log(`Successfully started the server at port: ${PORT}`)
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Successfully started the server at port: ${ServerConfig.PORT}`)
 })
