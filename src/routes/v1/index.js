8a463c6c21b50f642848207de3f7f70877d7e99d
iraj259 <irajj.259@gmail.com>
2025-10-14 12:28:01 +0100

    first commit

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
+const {InfoController} = require('../../controllers')
 const router = express.Router()
 
-router.get('/info', (req,res)=>{
-    return res.json({msg:'ok'})
-})
+router.get('/info', InfoController.info)
 module.exports = router
