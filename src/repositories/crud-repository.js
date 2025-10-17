2abd456a89448991f481af2e80d03d49fc41754b
iraj259 <irajj.259@gmail.com>
2025-10-17 19:09:57 +0100

    db and CRUD done

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -32,7 +32,7 @@ class CrudRepository{
             const response = await this.model.findByPK(data)
             return response;
         } catch (error) {
-            Logger.error("Something went wrong in the CRUD repo: destroy ")
+            Logger.error("Something went wrong in the CRUD repo: get ")
             throw error;
         }
     }
@@ -41,7 +41,20 @@ class CrudRepository{
             const response = await this.model.findAll()
             return response;
         } catch (error) {
-            Logger.error("Something went wrong in the CRUD repo: destroy ")
+            Logger.error("Something went wrong in the CRUD repo: getAll ")
+            throw error;
+        }
+    }
+    async update(id, data){
+        try {
+            const response = await this.model.update(data,{
+                where:{
+                    id:id
+                }
+            })
+            return response;
+        } catch (error) {
+            Logger.error("Something went wrong in the CRUD repo: update ")
             throw error;
         }
     }
