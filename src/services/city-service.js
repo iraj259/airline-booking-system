b91562a313d08dc84bf63db7efb941de02b95f42
iraj259 <irajj.259@gmail.com>
2025-10-20 19:15:06 +0100

    Cities added

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -3,3 +3,23 @@ const {CityRepository} = require('../repositories')
 const AppError = require('../utils/errors/app-error')
 
 const cityRepository = new CityRepository()
+
+async function createCity(data){
+try {
+        const city = await cityRepository.create(data)
+        return airplane
+    } catch (error) {
+        if(error.name = 'SequelizeValidationError'){
+            let explanation = []
+            error.errors.forEach((err)=>{
+                explanation.push(err.message)
+            })
+            throw new AppError("cannot create a new city object",StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+            throw new AppError("cannot create a new city object",StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
+
+module.exports={
+    createCity
+}
