d4b55b42f2659e27567638cda8f719e307ee5e54
iraj259 <irajj.259@gmail.com>
2025-10-21 17:12:52 +0100

    Flights added

--- src/index.js
+++ src/index.js
@@ -7,17 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api',apiRoutes)
 
-app.listen(ServerConfig.PORT,async()=>{
+app.listen(ServerConfig.PORT,()=>{
     console.log(`Successfully started the server at port: ${ServerConfig.PORT}`)
 
-    // bad code alert
-    const {City, Airport} = require('./models')
-    const city = await City.findByPk(1)
-    console.log(city)
-    // const airport = await Airport.create({name:'Lisbon airport', code:'Lisbon', cityId:1})
-await city.destroy({
-    where:{
-        id:1
-    }
-})
+    
 })
