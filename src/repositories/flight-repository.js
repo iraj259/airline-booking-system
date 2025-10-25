a4b4e91c49484154eb1388c6ed5c0c9b50320e3a
iraj259 <irajj.259@gmail.com>
2025-10-25 12:20:01 +0100

    Seats added

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,5 @@
  const CrudRepository = require('./crud-repository')
-const {Flight, Airplane,Airport} = require('../models')
+const {Flight, Airplane,Airport,City} = require('../models')
 const {Sequelize} = require('sequelize')
 class FlightRepository extends CrudRepository{
     constructor(){
@@ -21,6 +21,10 @@ class FlightRepository extends CrudRepository{
                 as:'departureAirport',
                 on:{
                     col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                },
+                include:{
+                    model:City,
+                    required:true
                 }
             },
             {
@@ -29,6 +33,10 @@ class FlightRepository extends CrudRepository{
                 as:'arrivalAirport',
                 on:{
                     col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                },
+                 include:{
+                    model:City,
+                    required:true
                 }
             } 
         ]
